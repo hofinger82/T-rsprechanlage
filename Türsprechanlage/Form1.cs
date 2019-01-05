c5d63968a47f57dc3391eb3bd21bba44fb76ae64
hofinger82 <46281568+hofinger82@users.noreply.github.com>
2019-01-05 01:29:01 +0100

    Config Files

--- Türsprechanlage/Form1.cs
+++ Türsprechanlage/Form1.cs
@@ -8,123 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using Newtonsoft.Json;
+
 
 namespace Türsprechanlage
 {
 public partial class Form1 : Form
     {
-        public static string MenuFilePath = "%home%/SweetHomeCP/Menu.json";
-        public static string SensorFilePath = "%home%/SweetHomeCP/sensor.json";
-        public static string SwitchFilePath = "%home%/SweetHomeCP/switch.json";
+        static string homePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+        public static string MenuFilePath = homePath + "/SweetHomeCP/Menu.conf";
+        
 
-        public string[,] MenuModes = new string[,] { 
-            {"Main", "Licht", "Tür", "Kameras", "Schildis", "Einstellungen", "menu" },
-            {"Sensoren", "Aussentemperatur", "Innentemperatur", "Temperatur Schildkrötenhaus", "Luftfeuchtigkeit aussen", "Wind", "mqtt"},
-            {"Licht", "Wohnzimmer Stehlampe", "Terasse", "Terasse Bodenleuchten", "", "", "mqtt"},
-            {"Schildkröten", "UV-Licht", "Wärmelampe", "Frostwächter", "Heizmatte T&L", "mehr...", "mqtt"},
-            {"Schildkröten 2", "Heizmatte Baby", "Tür vorne", "Tür mitte", "Tür hinten", "" , "mqtt"}
-           
-        };
+        public string[,] MenuModes = new string[10,10];
 
         static public string MenuMode = "Main";
-        int MenuPos = 0;
-        
-        
-
         public string[] subTopics;
 
-        public Dictionary<string,string> MQTTstatus = new Dictionary<string,string>();
-        public Dictionary<string, string> MenuDict = new Dictionary<string, string>();
-        public Dictionary<string, string> SensorDict = new Dictionary<string, string>();
-        public Dictionary<string, string> SwitchDict = new Dictionary<string, string>();
-
-        public class MenuType
-        {
-            public string Name { get; set; }
-            public int pos { get; set; }
-            public string[] MenuItems { get; set; }
-        }
-
-        public DataSet MenuDataSet;
-        public DataTable MenuDataTable;
+        static public Dictionary<string,string> MQTTstatus = new Dictionary<string,string>();
 
         public Form1()
         {
             InitializeComponent();
-
-            string jsonText = File.ReadAllText(MenuFilePath); //////////////// read menuitems from file and deserialize
-            //MenuDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
-            //men = JsonConvert.DeserializeObject<MenuType>(jsonText);
-            MenuDataSet = JsonConvert.DeserializeObject<DataSet>(jsonText);
-            MenuDataTable = MenuDataSet.Tables["MenuItem"];
-
-            jsonText = File.ReadAllText(SensorFilePath); //////////////// read sensors from file and deserialize into dictionary
-            SensorDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
-
-            jsonText = File.ReadAllText(SwitchFilePath); //////////////// read switches from file and deserialize into dictionary
-            SwitchDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
+            getSettings();
+            MqttManagement.readConfig();
 
             UpdateMenuItems();
-            List<string> subTopicList = new List<string>();
-
-            for (int i = 0; i < MenuModes.GetLength(0); i++)
-            {
-                if (MenuModes[i, MenuModes.GetLength(1) - 1] == "mqtt")
-                {
-                    for (int j = 1; j < MenuModes.GetLength(1); j++)
-                    {
-                        subTopicList.Add("/stat/" + MenuModes[i,0] + "/" +  MenuModes[i, j]);
-                        MQTTstatus[MenuModes[i,0] + "/" + MenuModes[i, j]] = "";
-                    }
-                }
-            }
-            subTopics = subTopicList.ToArray();
-
-            MqttManagement.setSubTopics(subTopics);
             
         }
 
         public void UpdateMenuItems()
         {
-            if (!MenuDict.ContainsKey(MenuMode))
-                return;
-
+            
             labelPage.Text = MenuMode;
 
-           
-
-           // if(MenuDataTable.Rows.Count )
 
 
-            foreach(DataRow row in MenuDataTable.Rows)
+            for (int i = 0; i < MenuModes.GetLength(0); i++)
             {
-                if(row["Name"] == MenuMode)
+                if (MenuModes[i, 1] == MenuMode)
                 {
-                    
-                    button1.Text = row["MenuItems"].ToString();
+                    labelPage.Text = MenuModes[i, 1];
+                    button1.Text = MenuModes[i, 1];
+                    //labelStatus1.Text = MQTTstatus[labelPage.Text + "/" + button1.Text];
+                    button2.Text = MenuModes[i, 2];
+                    //labelStatus2.Text = MQTTstatus[labelPage.Text + "/"  + button2.Text];
+                    button3.Text = MenuModes[i, 3];
+                    //labelStatus3.Text = MQTTstatus[labelPage.Text + "/"  + button3.Text];
+                    button4.Text = MenuModes[i, 4];
+                    //labelStatus4.Text = MQTTstatus[labelPage.Text + "/"  + button4.Text];
+                    button5.Text = MenuModes[i, 5];
+                    //labelStatus5.Text = MQTTstatus[labelPage.Text + "/"  + button5.Text];
 
                 }
             }
-            //for(int i = 0; i < MenuModes.GetLength(0); i++)
-            //{
-            //    if(MenuModes[i,0] == MenuMode)
-            //    {
-            //        labelPage.Text = MenuModes[i, 0];
-            //        button1.Text = MenuModes[i, 1];
-            //        //labelStatus1.Text = MQTTstatus[labelPage.Text + "/" + button1.Text];
-            //        button2.Text = MenuModes[i, 2];
-            //        //labelStatus2.Text = MQTTstatus[labelPage.Text + "/"  + button2.Text];
-            //        button3.Text = MenuModes[i, 3];
-            //        //labelStatus3.Text = MQTTstatus[labelPage.Text + "/"  + button3.Text];
-            //        button4.Text = MenuModes[i, 4];
-            //        //labelStatus4.Text = MQTTstatus[labelPage.Text + "/"  + button4.Text];
-            //        button5.Text = MenuModes[i, 5];
-            //        //labelStatus5.Text = MQTTstatus[labelPage.Text + "/"  + button5.Text];
 
-            //    }
-            //}
-            
         }
 
         
@@ -145,18 +81,32 @@ public partial class Form1 : Form
                 return;
             }
             
-            
-                if(MenuDict.ContainsKey(buttonText)) // wenn vorhanden, rufe ein untermenü auf
-                {
-                    MenuMode = buttonText;
-                    UpdateMenuItems();
-                    return;
-                }
 
+            for (int i = 0; i < MenuModes.GetLength(0); i++)
+            {
+                if (MenuModes[i, 0] == "menu")
+                {
+                    if (MenuModes[i, 1] == buttonText) // wenn vorhanden, rufe ein untermenü auf
+                    {
+                        MenuMode = buttonText;
+                        UpdateMenuItems();
+                        return;
+                    }
+                }
+                else if(MenuModes[i,0] == "switch")
+                {
+                    string mqttTopic = MqttManagement.getPubTopic(buttonText);
+                    if(mqttTopic != "FAIL")
+                        MqttManagement.PublishMqttMessage(mqttTopic, "TOGGLE"); //ansonsten mqtt publish
+                }
+                else if (MenuModes[i, 0] == "sensor")
+                {
+
+                }
+            }
             
             
-            string mqttTopic = "/" + MenuMode + "/" + buttonText;
-            MqttManagement.PublishMqttMessage(mqttTopic, "TOGGLE"); //ansonsten mqtt publish
+            
         }
 
         
@@ -183,16 +133,39 @@ public partial class Form1 : Form
 
         public static void UpdateMqttStatus(Dictionary<string,string> status)
         {
-            string mode;
-            try { mode = status[MenuMode];
-                    }
-            catch (Exception) { MessageBox.Show("menumode not found: " + MenuMode); }
+            MQTTstatus = status;
+            UpdateLabels();
         }
+
+        public void UpdateLabels()
+        {
+            Form1.
+        }
+
 
         private void buttonLeft_Click(object sender, EventArgs e)
         {
             MenuMode = "Main";
             UpdateMenuItems();
         }
+
+        private void getSettings()
+        {
+            //MenuModes
+            int x = 0;
+            foreach (string line in File.ReadLines(MenuFilePath))
+            {
+                int y = 0;
+                foreach (string item in line.Split(','))
+                {
+                    MenuModes[x, y] = item;
+                    y++;
+                }
+                x++;
+            }
+
+        }
+
+        
     }
 }
--- Türsprechanlage/MqttManagement.cs
+++ Türsprechanlage/MqttManagement.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
+using System.IO;
 
 namespace Türsprechanlage
 {
@@ -15,9 +16,39 @@ namespace Türsprechanlage
         static string MqttStatusText;
         static string[] subTopics;
 
-        public static void setSubTopics(string[] topics)
+        static string homePath = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+        public static string SensorFilePath = homePath + "/SweetHomeCP/sensor.json";
+        public static string SwitchFilePath = homePath + "/SweetHomeCP/switch.json";
+        public static string[,] Switches = new string[20, 7];
+
+        static Dictionary<string,string> status = new Dictionary<string,string>();
+
+        public static void readConfig()
         {
-            subTopics = topics;
+            //Switches
+            int x = 0;
+            foreach (string line in File.ReadLines(SwitchFilePath))
+            {
+                int y = 0;
+                foreach (string item in line.Split(','))
+                {
+                    Switches[x, y] = item;
+                    y++;
+                }
+                x++;
+            }
+        }
+    
+
+        public static void setSubTopics()
+        {
+            List<string> subTList = new List<string>();
+            for(int i = 0; i < Switches.GetLength(0); i++)
+            {
+                subTList.Add(Switches[i, 3]);
+            }
+            subTopics = subTList.ToArray();
+                
         }
 
         public static void ReconnectMQTT()
@@ -39,6 +70,7 @@ namespace Türsprechanlage
             {
                 MqttStatusText = "connected";
                 //labelMqttStatus.ForeColor = Color.Green;
+                setSubTopics();
                 ushort msgId = client.Subscribe(
                     subTopics,
                     new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
@@ -52,19 +84,26 @@ namespace Türsprechanlage
             //labelMqttStatus.Text = "MQTT Status: " + MqttStatusText;
         }
 
-        public string getStatus()
+        static public string getStatus()
         {
             return MqttStatusText;
+        }
+
+        static public string getStatus(string buttonText)
+        {
+
         }
 
         static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
         {
             //Debug.WriteLine("Received = " + Encoding.UTF8.GetString(e.Message) + " on topic " + e.Topic);
-            string strStatus;
-            strStatus = e.Topic;
-            strStatus = strStatus.Replace("/stat/", "");
-            Dictionary<string, string> status = new Dictionary<string, string>();
-            status[strStatus] = e.Message.ToString();
+            for(int i = 0; i < Switches.GetLength(0); i++)
+            {
+               if(Switches[i,3] == e.Topic)
+                {
+                    status[Switches[i, 1]] = Encoding.UTF8.GetString(e.Message);
+                }
+            }
             Form1.UpdateMqttStatus(status);
         }
 
@@ -73,6 +112,18 @@ namespace Türsprechanlage
             int errCode;
             errCode = client.Publish(topic, Encoding.UTF8.GetBytes(payload));
             return errCode;
+        }
+
+        static public string getPubTopic(string buttonText)
+        {
+            for(int i = 0; i < Switches.GetLength(0); i++)
+            {
+                if(Switches[i,1] == buttonText)
+                {
+                    return Switches[i, 2];
+                }
+            }
+            return "FAIL";
         }
     }
 }
